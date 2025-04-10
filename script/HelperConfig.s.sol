07dc12be47d72bd45b97f16c0d2b3eb8194dcd53
Avinash <cryptocapinoid@gmail.com>
2025-04-10 13:22:09 +0530

    Update HelperConfig.s.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -18,10 +18,10 @@ contract HelperConfig is Script {
       uint8 public constant DECIMALS = 8;
       int256 public constant ETH_USD_PRICE = 2000e8;
       int256 public constant BTC_USD_PRICE = 1000e8;
-      int256 public DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+      int256 public DEFAULT_ANVIL_KEY = YOUR_DEFAULT_ANVIL_KEY;
 
 
-      NetworkConfig public activeNetworkConfig;
+      NetworkConfig public activeNetworkConfig; // stores active configuration
 
       constructor() {
         if(block.chainid == 11155111){
@@ -37,7 +37,7 @@ contract HelperConfig is Script {
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
             wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
-            deployerKey: vm.envUint("PRIVATE_KEY")
+            deployerKey: vm.envUint("PRIVATE_KEY") // loading deployers private key from .env
         });
       }
 
